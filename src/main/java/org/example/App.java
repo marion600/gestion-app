1e0237b9236585c711961b4fd21d7448c4afd5eb
marion600 <ngnobanawel29@gmail.com>
2026-01-31 17:13:22 +0100

    Liason de gestion-app à cartographie-lib

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,14 +7,15 @@ import fr2.cours.info.tp.ferroviaire.cartographie.models.Gare;
 
 public class App {
     public static void main(String[] args) {
+// Création d'une gare
+        Gare gareB = new Gare();
+        gareB.setNom("Gare B");
+        gareB.setNombreQuais(1);
 
-        Gare gare = new Gare(
-                "GARE-001",
-                "Le Mans",
-                3,
-                1
-        );
+        System.out.println("Gare créée : " + gareB.getNom());
+        System.out.println("Nombre de quais : " + gareB.getNombreQuais());
 
-        System.out.println(gare.getNom());
+        // Les chemins de fer / voies seront ajoutés plus tard
+        // quand tu auras créé les classes correspondantes
     }
 }

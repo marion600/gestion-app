db11f3fac3ce4607c81b32b97268552dc96817dd
marion600 <ngnobanawel29@gmail.com>
2026-01-31 17:50:43 +0100

    Génération du code de l'api REST de gestion-app

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- */
 import fr2.cours.info.tp.ferroviaire.cartographie.models.Gare;
 
 public class App {
     public static void main(String[] args) {
-// Création d'une gare
-        Gare gareB = new Gare();
-        gareB.setNom("Gare B");
-        gareB.setNombreQuais(1);
 
-        System.out.println("Gare créée : " + gareB.getNom());
-        System.out.println("Nombre de quais : " + gareB.getNombreQuais());
+        Gare gare = new Gare(
+                "G1",
+                "Gare Centrale",
+                2,
+                101
+        );
 
-        // Les chemins de fer / voies seront ajoutés plus tard
-        // quand tu auras créé les classes correspondantes
+        System.out.println(gare.getNom());
     }
 }
